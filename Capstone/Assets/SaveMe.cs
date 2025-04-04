c1bf1489b425a7d343e074e519c2b74be0e841ee
TomMazor2 <mazort@mymacewan.ca>
2025-04-04 04:17:02 -0600

    Final Version
    Final Working Version:
    
    Note: For presentations sake, I took out the calming response in the plank level because it was bugged

---
+++ Capstone/Assets/SaveMe.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class SaveMe : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
